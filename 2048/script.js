f55b40dc6006b9c3f53e97729aa08861c4f56dd3
JohnChen <paiching01@gmail.com>
2020-09-29 07:02:14 +0800

    update

--- 2048/script.js
+++ 2048/script.js
@@ -1,8 +1,11 @@
 let myApp = new Vue({
     el: '#myApp',
-    data: {},
+    data: {
+        squares: [],
+    },
     mounted: function () {
         this.createBoard();
+        this.moveLeft()
     },
     methods: {
         createBoard: function () {
@@ -10,14 +13,69 @@ let myApp = new Vue({
             const scoreDisplay = document.getElementById('score')
             const resultDisplay = document.getElementById('result')
             const width = 4
-            let squares = []
+            // let squares = []
 
             for (let i = 0; i < width * width; i++) {
                 let square = document.createElement('div')
                 square.innerHTML = 0;
                 gridDisplay.appendChild(square)
-                squares.push(square)
+                this.squares.push(square)
             }
+            this.generate()
+            this.generate()
+        },
+        generate: function () {  // generate a number randomly
+            let randomNumber = Math.floor(Math.random() * this.squares.length)
+            if (this.squares[randomNumber].innerHTML === '0') {  // 0 is a string NOT number
+                this.squares[randomNumber].innerHTML = 2
+            } else this.generate()
+        },
+        moveRight: function () {  // swipe right
+            for (let i = 0; i < 16; i++) {
+                if (i % 4 === 0) {
+                    let totalOne = this.squares[i].innerHTML
+                    let totalTwo = this.squares[i + 1].innerHTML
+                    let totalThree = this.squares[i + 2].innerHTML
+                    let totalFour = this.squares[i + 3].innerHTML
+                    let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+                    let filteredRow = row.filter(num => num)  // 把 > 0 的傳出
+                    let missing = 4 - filteredRow.length
+                    let zeros = Array(missing).fill(0)  // 使用固定值填充陣列，可選START、END位置
+                    let newRow = zeros.concat(filteredRow)
+
+                    // 數字都會靠右
+                    this.squares[i].innerHTML = newRow[0]
+                    this.squares[i + 1].innerHTML = newRow[1]
+                    this.squares[i + 2].innerHTML = newRow[2]
+                    this.squares[i + 3].innerHTML = newRow[3]
+                }
+            }
+        },
+        moveLeft: function () {  // swipe left
+            for (let i = 0; i < 16; i++) {
+                if (i % 4 === 0) {
+                    let totalOne = this.squares[i].innerHTML
+                    let totalTwo = this.squares[i + 1].innerHTML
+                    let totalThree = this.squares[i + 2].innerHTML
+                    let totalFour = this.squares[i + 3].innerHTML
+                    let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+                    let filteredRow = row.filter(num => num)  // 把 > 0 的傳出
+                    let missing = 4 - filteredRow.length
+                    let zeros = Array(missing).fill(0)  // 使用固定值填充陣列，可選START、END位置
+                    let newRow = filteredRow.concat(zeros);
+
+                    // 數字都會靠右
+                    this.squares[i].innerHTML = newRow[0]
+                    this.squares[i + 1].innerHTML = newRow[1]
+                    this.squares[i + 2].innerHTML = newRow[2]
+                    this.squares[i + 3].innerHTML = newRow[3]
+                }
+            }
+        },
+        combineRow: function () {
+
         }
     }
 })
