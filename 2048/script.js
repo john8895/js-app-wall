c85ac9e9dd045bdb36a99d76eb220132767140a3
JohnChen <paiching01@gmail.com>
2020-09-30 07:03:27 +0800

    update

--- 2048/script.js
+++ 2048/script.js
@@ -2,6 +2,8 @@ let myApp = new Vue({
     el: '#myApp',
     data: {
         squares: [],
+        width: 4,
+        resultDisplay: '',
     },
     mounted: function () {
         this.createBoard();
@@ -12,11 +14,11 @@ let myApp = new Vue({
         createBoard: function () {
             const gridDisplay = document.querySelector('.grid');
             const scoreDisplay = document.getElementById('score')
-            const resultDisplay = document.getElementById('result')
-            const width = 4
+            // const resultDisplay = document.getElementById('result')
+            // const width = 4
             // let squares = []
 
-            for (let i = 0; i < width * width; i++) {
+            for (let i = 0; i < this.width * this.width; i++) {
                 let square = document.createElement('div')
                 square.innerHTML = 0;
                 gridDisplay.appendChild(square)
@@ -75,6 +77,44 @@ let myApp = new Vue({
                 }
             }
         },
+        moverDown: function () {
+            for (var i = 0; i < 4; i++) {
+                let totalOne = this.squares[i].innerHTML
+                let totalTwo = this.squares[i + this.width].innerHTML
+                let totalThree = this.squares[i + (this.width * 2)].innerHTML
+                let totalFour = this.squares[i + (this.width * 3)].innerHTML
+                let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+                let filteredColumn = column.filter(num => num)
+                let missing = 4 - filteredColumn.length
+                let zeros = Array(missing).fill(0)
+                let newColumn = zeros.concat(filteredColumn)
+
+                this.squares[i].innerHTML = newColumn[0]
+                this.squares[i + this.width].innerHTML = newColumn[1]
+                this.squares[i + (this.width * 2)].innerHTML = newColumn[2]
+                this.squares[i + (this.width * 3)].innerHTML = newColumn[3]
+            }
+        },
+        moverUp: function () {
+            for (var i = 0; i < 4; i++) {
+                let totalOne = this.squares[i].innerHTML
+                let totalTwo = this.squares[i + this.width].innerHTML
+                let totalThree = this.squares[i + (this.width * 2)].innerHTML
+                let totalFour = this.squares[i + (this.width * 3)].innerHTML
+                let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+                let filteredColumn = column.filter(num => num)
+                let missing = 4 - filteredColumn.length
+                let zeros = Array(missing).fill(0)
+                let newColumn = filteredColumn.concat(zeros)
+
+                this.squares[i].innerHTML = newColumn[0]
+                this.squares[i + this.width].innerHTML = newColumn[1]
+                this.squares[i + (this.width * 2)].innerHTML = newColumn[2]
+                this.squares[i + (this.width * 3)].innerHTML = newColumn[3]
+            }
+        },
         combineRow: function () {
             for (let i = 0; i < 15; i++) {
                 if (this.squares[i].innerHTML === this.squares[i + 1].innerHTML) {
@@ -83,12 +123,27 @@ let myApp = new Vue({
                     this.squares[i + 1].innerHTML = 0;
                 }
             }
+            this.checkForWin()
+        },
+        combineColumn: function () {
+            for (let i = 0; i < 12; i++) {
+                if (this.squares[i].innerHTML === this.squares[i + this.width].innerHTML) {
+                    let combinedTotal = parseInt(this.squares[i].innerHTML) + parseInt(this.squares[i + this.width].innerHTML);
+                    this.squares[i].innerHTML = combinedTotal;
+                    this.squares[i + this.width].innerHTML = 0;
+                }
+            }
+            this.checkForWin()
         },
         control: function (e) {
             if (e.keyCode === 39) {
                 this.keyRight();
             } else if (e.keyCode === 37) {
                 this.keyLeft();
+            } else if (e.keyCode === 38) {
+                this.keyUp()
+            } else if (e.keyCode === 40) {
+                this.keyDown()
             }
         },
         keyRight: function () {
@@ -103,6 +158,29 @@ let myApp = new Vue({
             this.moveLeft();
             this.generate();
 
-        }
-    }
+        },
+        keyDown: function () {
+            this.moverDown()
+            this.combineColumn()
+            this.moverDown()
+            this.generate()
+        },
+        keyUp: function () {
+            this.moverUp()
+            this.combineColumn()
+            this.moverUp()
+            this.generate()
+        },
+        checkForWin: function () {
+            for (let i = 0; i < this.squares.length; i++) {
+                if (this.squares[i].innerHTML === 2048){
+                    this.resultDisplay = 'You Win!'
+                    document.removeEventListener('keyUp', this.control)
+                }
+            }
+        },
+        checkForGameOver: function() {
+            let zeros = 0
+
+        }    }
 })
