82ea0d76b19fb6e4a203ce4b502a6419c1ba8aa5
JohnChen <paiching01@gmail.com>
2020-10-06 06:59:38 +0800

    update

--- clock-with-changing-background2/script.js
+++ clock-with-changing-background2/script.js
@@ -3,6 +3,7 @@
 let hourHand = document.querySelector('.hour'),
     minuteHand = document.querySelector('.minute'),
     secondHand = document.querySelector('.second')
+
 rotate();
 function rotate() {
     const currentDate = new Date()
@@ -15,6 +16,7 @@ function rotate() {
     const minutesFraction = (secondsFraction + minutes) / 60
     const hoursFraction = (minutesFraction + hours) / 12
     // rotating fraction --> how many fraction to rotate for each hand.
+    // 時針、分針都跟秒針一樣是一小格一小格走，而不是一次走一個小時，所以這邊要減慢行走的速度
     // 以減慢相對於彼此的手的速度
     // const secondsFraction = seconds / 60;
     // const minutesFraction = minutes / 60;
@@ -24,4 +26,15 @@ function rotate() {
     const secondsRotate = secondsFraction * 360
     const minutesRotate = minutesFraction * 360
     const hoursRotate = hoursFraction * 360
+
+    // apply the rotate style to each element
+    // use backtick `` instead of single quotes ''
+
+    secondHand.style.transform = `rotate(${secondsRotate}deg)`
+    minuteHand.style.transform = `rotate(${minutesRotate}deg)`
+    hourHand.style.transform = `rotate(${hoursRotate}deg)`
+
 }
+
+// for every 1000 milliseconds(ie, 1 second) interval, activate the rotate() function.
+setInterval(rotate, 1000)
