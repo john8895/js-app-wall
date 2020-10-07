9fbd942a001bfb1c61a3877038ddf1eaf5ce7505
JohnChen <paiching01@gmail.com>
2020-10-08 07:05:10 +0800

    calculator

--- base-project/script.js
+++ base-project/script.js
@@ -1,8 +1,10 @@
 const buttons = document.querySelectorAll('button')
 const display = document.querySelector('.display')
 
-buttons.forEach(button => {button.addEventListener('click',calculator)})
+buttons.forEach(button => {
+    button.addEventListener('click', calculator)
+})
 
-function calculator(){
+function calculator(event) {
 
 }
