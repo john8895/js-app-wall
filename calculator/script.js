5ae1fc501e4e5e4ae6d98e7df61e46c9cbd604fa
john8895 <paiching01@gmail.com>
2020-10-08 09:06:50 +0800

    calculator

---
+++ calculator/script.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll('button')
+const display = document.querySelector('.display')
+
+buttons.forEach(button => {
+    button.addEventListener('click', calculator)
+})
+
+function calculator(event) {
+
+}
